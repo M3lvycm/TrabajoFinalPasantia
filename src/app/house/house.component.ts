eb7c9308fc0cec72c9e6c5b4ae33c47e8c56bea1
M3lvycm <melvisyaelc@gmail.com>
2025-04-10 11:16:49 -0400

    fix(house.component): add missing house details for Lincoln entry
    The Lincoln entry in the house array was missing the `nH`, `mC`, and `nG` properties. This commit adds these properties to ensure consistency across all house entries.

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -15,7 +15,7 @@ export class HouseComponent {
   house = [
     { title: 'La 27 de febrero', text: 'Av. 27 de Febrero #429, Santo Domingo.', img: '501673-arenas-de-barcelona.webp', nH: "3", mC:'120', nG: '2'  },
     { title: 'U.S.A', text: 'Av. George Washington #1, Santo Domingo.', img: 'About_the_USA_NYC_Statue_Liberty_._CROP_Web72DPI.jpg', nH: "3", mC:'120', nG: '2' },
-    { title: 'Lincoln', text: 'Av. Abraham Lincoln, Santo Domingo.', img: 'K71TBD.webp',  },
+    { title: 'Lincoln', text: 'Av. Abraham Lincoln, Santo Domingo.', img: 'K71TBD.webp', nH: "3", mC:'120', nG: '2'  },
     { title: 'San Vicente', text: 'Av. San Vicente de Paúl, Santo Domingo Este.', img: 'photo-1502602898657-3e91760cbb34.jpg', nH: "3", mC:'120', nG: '2'  },
 
   ];
