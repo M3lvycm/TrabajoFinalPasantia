6397f98d87c93f9b4d08b4c1eb1de6ee4685062e
Hilde Cabrera <HJCA3120@gmail.com>
2025-04-10 14:29:24 -0400

    Creacion de la seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from "./register/register.component";
 import { CitysComponent } from "./citys/citys.component";
 import { HouseComponent } from "./house/house.component";
-import { NavbarComponent } from './navbar/navbar.component';;
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section4Component } from './section-4/section-4.component';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CitysComponent,HouseComponent],
+  imports: [RouterOutlet, NavbarComponent, CitysComponent, HouseComponent, Section4Component, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
