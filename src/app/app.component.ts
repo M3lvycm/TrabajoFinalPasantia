291119e7c29c321619a29542cbd9d45542f6d151
Hilde Cabrera <HJCA3120@gmail.com>
2025-04-10 12:00:52 -0400

    navbar changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HeroSectionComponent, NavbarComponent],
-=======
 import { LoginService } from './Service/login.service';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from "./register/register.component";
 import { CitysComponent } from "./citys/citys.component";
 import { HouseComponent } from "./house/house.component";
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CitysComponent, HouseComponent],
+  imports: [RouterOutlet, CitysComponent, HouseComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
